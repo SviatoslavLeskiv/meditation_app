8f3c17d7c3cf19b21d126fe349d40c90d6e621f5
Sviatoslav Leskiv <sviatoslav.leskiv@beleven.dev>
2023-12-14 20:16:44 +0200

    Create bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app/presentation/tasks/tasks.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'presentation/login/login.dart';
+import 'presentation/posts/posts.dart';
+import 'presentation/profile/profile.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,16 +14,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: LoginPage(),
+        body: LoginPage(
+          isLogout: false,
+        ),
       ),
     );
   }
 }
 
 class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
+  final SharedPreferences preferences;
+
+  const BottomNavigationBarExample({
+    super.key,
+    required this.preferences,
+  });
 
   @override
   State<BottomNavigationBarExample> createState() =>
@@ -29,22 +41,19 @@ class BottomNavigationBarExample extends StatefulWidget {
 class _BottomNavigationBarExampleState
     extends State<BottomNavigationBarExample> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Tasks',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Profile',
-      style: optionStyle,
-    ),
-  ];
+  late List<Widget> _widgetOptions;
+
+  @override
+  void initState() {
+    _widgetOptions = <Widget>[
+      const PostsPage(),
+      const TasksPage(),
+      ProfilePage(
+        preferences: widget.preferences,
+      ),
+    ];
+    super.initState();
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -57,7 +66,7 @@ class _BottomNavigationBarExampleState
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        color: Color(0xFF589AAF).withOpacity(0.1),
+        color: const Color(0xFF589AAF).withOpacity(0.1),
         child: Center(
           child: _widgetOptions.elementAt(_selectedIndex),
         ),
